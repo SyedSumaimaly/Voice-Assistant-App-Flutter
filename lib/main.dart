a96c939c747da945cf15991ee2695b7ff995b466
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-12-02 07:25:33 +0500

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:voice_assistant_app/home_page.dart';
+import 'package:voice_assistant_app/pallete.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Voice Assistant App',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
